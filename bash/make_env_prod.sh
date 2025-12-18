ca76f25004bd09673c0657e8e4ec04ac6d2def81
stanislavpy934 <stanislavpy934@proton.me>
2025-12-18 11:55:55 +0200

    minor improvements

--- bash/make_env_prod.sh
+++ bash/make_env_prod.sh
@@ -8,13 +8,13 @@ echo "Creating .env file with production settings..."
 cat > .env << 'EOF'
 SECRET_KEY=SYfb49obs_gPwqGVcvooAcake8Bg1Au8Zdoe2NwQ8L-pu8O-uQ1WKyIhp-zDjLmMBTQ
 DEBUG=False
-ALLOWED_HOSTS=damir96.pythonanywhere.com,www.damir96.pythonanywhere.com
+ALLOWED_HOSTS=stanislavpy934.pythonanywhere.com,www.stanislavpy934.pythonanywhere.com
 
 DB_ENGINE=django.db.backends.sqlite3
-DB_NAME=/home/damir96/jobs-portal-django/db.sqlite3
+DB_NAME=/home/stanislavpy934/jobs-portal-django/db.sqlite3
 
 DJANGO_SETTINGS_MODULE=jobs_portal.settings.prod
-CSRF_TRUSTED_ORIGINS=https://damir96.pythonanywhere.com,http://damir96.pythonanywhere.co
+CSRF_TRUSTED_ORIGINS=https://stanislavpy934.pythonanywhere.com,http://stanislavpy934.pythonanywhere.co
 EOF
 
 echo "Done! .env file created with production settings."
